64b7b2d203f1e332f49227724f1d8a098cc63f49
MadakariNayakaHM <madakarinayakadinesh6282@gmail.com>
2026-06-12 15:53:44 +0530

    Var-let Understanding

---
+++ core-js/index.js
@@ -0,0 +1,19 @@
+console.log("hi from core-js");
+
+// js is a synchronous , single threaded  language
+
+// var msg = "hello";
+// var msg = "hi";
+// console.log(msg); // hi
+
+// var allows us to reassign and redeclare variables    
+
+// console.log(msg); // undefined
+// var msg = "hello";
+
+
+// console.log(msg); // ReferenceError: Cannot access 'msg' before initialization
+// let msg = "hello";  // scriot creates a temporal dead zone for let and const variables
+// let allows us to reassign but not redeclare variables    
+
+
