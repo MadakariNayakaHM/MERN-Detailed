63734cd3c60d6a6da0fbf07c0853b3dcebb812fd
MadakariNayakaHM <madakarinayakadinesh6282@gmail.com>
2026-06-12 22:11:55 +0530

    added diff kind of function

--- core-js/index.js
+++ core-js/index.js
@@ -78,3 +78,47 @@ console.log("hi from core-js");
 
 // greet(); // added to call stack
 
+// Annonymus function 
+
+// const greet = function() // it will be initialised in script scope and assigned to greet variable  
+// {
+//     var a = 1  // local memory context will be created for all the variable inside it
+//     console.log("hi");
+// }
+
+// greet();  // added to call stack
+
+// callback function : a function that is passed as an argument to another function and is executed after some operation is completed.
+
+
+// function x(cb)
+// {
+//     console.log("hi from x");
+
+//     cb();
+// }
+
+// function y()
+// {
+//     console.log("hi from y");
+// }
+
+// x(y); // hi from x, hi from y
+
+// both x and y initialised in global scope, x is added to call stack, x is executed and it calls y, y is added to call stack and executed.
+
+// IIFE : Immediately Invoked Function Expression : a function that is executed immediately after it is defined. It is used to create a new scope and avoid polluting the global scope.
+
+// (
+// function greet()
+// {
+//     console.log("hi from IIFE");
+// }
+
+// )
+// ();  We cannot see a function mapped in the global scope because it is immediately invoked and executed, and then it is removed from the memory. It is not accessible outside of its own scope.
+
+
+// HOF : Higher Order Function : a function that takes another function as an argument or returns a function as a result. It is used to create reusable code and to abstract away common patterns.
+
+
